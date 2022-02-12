f8a534a7fce6b668098aa5127279f5df6e9dcd4c
Elijah Rivers <leeb467ma@gmail.com>
2022-02-11 21:04:52 -0500

    base files for matramino

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('#grid');
+    let boxes = Array.from(document.querySelectorAll('.box'));
+    const Score = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
+})
