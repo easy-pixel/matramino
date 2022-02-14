f36f2eb6d5d73c7363592c00484be7503573f0e7
Elijah Rivers <leeb467ma@gmail.com>
2022-02-13 23:42:55 -0500

    debug

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const Score = document.querySelector('#score');
     const StartBtn = document.querySelector('#start-button');
 })
+
+function init() {
+    for(x=0; x<200; x++) {
+        var box = document.createElement('div');
+        box.className = "box";
+        document.getElementById('grid').appendChild(box);
+    }
+}
